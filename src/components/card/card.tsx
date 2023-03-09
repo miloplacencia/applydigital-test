1b12b70eaeb9e1032e824bde0dec962cc640b573
miloplacencia <placenciameister@gmail.com>
2023-03-09 21:39:22 +0000

    FEAT: Working on Card component

---
+++ src/components/card/card.tsx
@@ -0,0 +1,50 @@
+import { relativeDate } from "../../helpers/date";
+import { ClockIcon } from "../icons/clock";
+import { HeartFullIcon } from "../icons/heart-full";
+import { HeartLineIcon } from "../icons/heart-line";
+import "./card.css";
+
+export default function CardComponent({
+  liked = false,
+  date = new Date(),
+  title,
+  link,
+  author = "Author",
+  onClickLike,
+}: {
+  liked?: boolean;
+  title?: string;
+  date?: Date;
+  author?: string;
+  link?: string;
+  onClickLike?: () => void;
+}) {
+  const formattedDate = date ? relativeDate(new Date(date)) : "";
+
+  return (
+    <div className="card">
+      <div className="card-content">
+        <div className="card-date">
+          <ClockIcon height="1rem" width="1rem" />
+          <span>
+            {formattedDate} by {author}
+          </span>
+        </div>
+        <p className="card-text">
+          <a href={link} rel="noopener noreferrer" target="_blank">
+            {title}
+          </a>
+        </p>
+      </div>
+      <div className="card-likebtn">
+        <button onClick={onClickLike}>
+          {liked ? (
+            <HeartFullIcon height="1.375rem" width="1.5rem" />
+          ) : (
+            <HeartLineIcon height="1.375rem" width="1.5rem" />
+          )}
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
+import CardComponent from "../components/card/card";
+
 export default function Index() {
   return (
     <div id="index" className="grid">
-      <div className="" style={{ height: 50, background: "#DDD" }}></div>
-      <div className="" style={{ height: 50, background: "#DDD" }}></div>
-      <div className="" style={{ height: 50, background: "#DDD" }}></div>
-      <div className="" style={{ height: 50, background: "#DDD" }}></div>
-      <div className="" style={{ height: 50, background: "#DDD" }}></div>
+      {Array.from({ length: 10 }).map(() => (
+        <CardComponent title="All the fundamental React.js concepts, jammed into this single Medium article (updated August 2019)" />
+      ))}
     </div>
   );
 }
