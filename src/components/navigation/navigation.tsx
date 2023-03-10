d4f2e93285e808b42778c5a53853361efa4bfb85
miloplacencia <placenciameister@gmail.com>
2023-03-10 00:41:36 +0000

    FEAT: Navigation with styles and functionality

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,10 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navigation() {
+  const location = useLocation();
+
+  const isLinkActive = (path: string) => location.pathname === path;
+
   return (
     <nav>
-      <Link to="/">All</Link>
-      <Link to="/favorites">My faves</Link>
+      <Link to="/" className={isLinkActive("/") ? "active" : ""}>
+        All
+      </Link>
+      <Link
+        to="/favorites"
+        className={isLinkActive("/favorites") ? "active" : ""}
+      >
+        My faves
+      </Link>
     </nav>
   );
 }
